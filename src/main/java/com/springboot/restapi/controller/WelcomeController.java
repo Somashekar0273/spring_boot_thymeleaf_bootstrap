659471900b6d7a6674836c2c6a82ab701bca3744
nandishkb <encodefox@gmail.com>
2019-01-10 13:08:12 +0530

    Initial commit using Thymeleaf and Bootstrap

---
+++ src/main/java/com/springboot/restapi/controller/WelcomeController.java
@@ -0,0 +1,20 @@
+package com.springboot.restapi.controller;
+
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class WelcomeController {
+
+	@Value("${welcome.message}")
+	private String message = "Bengaluru";
+
+	@RequestMapping("/")
+	public String welcome(Map<String, Object> model) {
+		model.put("message", message);
+		return "index";
+	}
+}
